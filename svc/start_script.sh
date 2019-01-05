d713f500e220c9d23698f2874ff7f79e5c57af7d
Georg Art <georgart@Georgs-MBP.lan>
2019-01-05 23:03:44 +0100

    Start script updated

--- svc/start_script.sh
+++ svc/start_script.sh
@@ -1,7 +1,4 @@
 #!/bin/sh -e
 
 git clone https://github.com/yart76/docker-rest-web.git
-cd svc
-vagrant up
-cd ansible
-ansible-playbook -vv -i inv_hosts main.yml --user vagrant -k -K
+[ cd svc ]] && [ vagrant up ]] && [ cd ansible ] && [ ansible-playbook -vv -i inv_hosts main.yml --user vagrant -k -K ]
